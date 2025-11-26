7aaf72d191430000e7d825a8187e5197deb6626f
AmonBrollo <AmonBrollo@users.noreply.github.com>
2025-11-26 01:34:31 +0000

    deploy: ef34aafd5aa07983d88f235321f8b7cb3bc6907d

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"13e658725ddaa270601426d1485636157e38c3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2053432956"
+    serviceWorkerVersion: "3345121493"
   }
 });
